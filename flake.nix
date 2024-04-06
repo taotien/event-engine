3d64d07c336bd453bb967f47ebd9f8d025563da6
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-04-05 20:36:58 -0700

    flake

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
         devShell = mkShell rec {
           buildInputs = [
             pkg-config
+            openssl
           ];
           LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
         };
