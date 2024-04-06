0ab1d6ffc59c1e2bed99bcbb63a0270b7af8c6a1
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-04-05 22:13:31 -0700

    Add transit method options

--- src/cli/src/main.rs
+++ src/cli/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use std::process::exit;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -14,6 +15,10 @@ enum Commands {
         /// Maximum allowed straight line distance
         #[arg(short, long)]
         radius: Option<u32>,
+
+        /// Transit methods: {walk|car|transit}
+        #[arg(short, long)]
+        method: Option<String>,
     },
 
     /// Update web crawler cache
@@ -24,9 +29,25 @@ fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Some(Commands::List { radius }) => {
+        Some(Commands::List { radius, method }) => {
             if let Some(radius) = radius {
                 println!("radius: {radius}");
+            }
+
+            if let Some(method) = method {
+                if method == "walk" {
+                    println!("Transit method: walking");
+                    /* TODO */
+                } else if method == "car" {
+                    println!("Transit method: car");
+                    /* TODO */
+                } else if method == "transit" {
+                    println!("Transit method: public transit");
+                    /* TODO */
+                } else {
+                    println!("Error: unsupported transit method");
+                    exit(-1);
+                }
             }
         }
         Some(Commands::Update {}) => todo!(),
